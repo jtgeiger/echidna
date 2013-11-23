3fee21f84eae0997b99c600cf6cb136b281b8c83
Joel Geiger <joel@alltooeasy.com>
2013-11-23 04:39:30 -0500

    Add TODO for the analyzer.

---
+++ src/main/java/com/alltooeasy/echidna/analysis/Analyzer.java
@@ -0,0 +1,13 @@
+package com.alltooeasy.echidna.analysis;
+
+public class Analyzer
+{
+
+    //TODO:
+    //loop through each cell
+    //get the row, col, and cluster
+    //make a list of candidates by removing any values already set
+    //if only one candidate left then we got it
+    //keep looping until solved or until no changes to grid or candidate list (algorithm fail)
+
+}
