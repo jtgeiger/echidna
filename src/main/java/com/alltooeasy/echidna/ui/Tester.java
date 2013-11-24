c88ff85fc0eac5d65a1a755e3c09fa917a8b3a8f
Joel Geiger <joel@alltooeasy.com>
2013-11-24 01:54:21 -0500

    Solved another one from websudoku.com.

--- src/main/java/com/alltooeasy/echidna/ui/Tester.java
+++ src/main/java/com/alltooeasy/echidna/ui/Tester.java
@@ -20,7 +20,7 @@ public class Tester
         log.info( "main() started." );
 
         //Grid g = new Grid();
-        Grid g = GridLoader.load( getClasspathResource( "/test2.txt" ) );
+        Grid g = GridLoader.load( getClasspathResource( "/test3.txt" ) );
         SudokuPanel panel = new SudokuPanel( g );
         GuiBuilder guiBuilder = new GuiBuilder( panel );
         guiBuilder.show();
